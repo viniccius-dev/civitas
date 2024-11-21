fcdd6efa7dfc5b810b78d3ab431e20da6a053204
Anna <acesbrama@gmail.com>
2024-11-21 03:14:53 -0300

    feat: continuação da criação das estruturas do formulário

--- src/app/pages/main/components/form-registration/form-registration.component.ts
+++ src/app/pages/main/components/form-registration/form-registration.component.ts
@@ -6,8 +6,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './form-registration.component.html',
   styleUrls: ['./form-registration.component.scss']
 })
+
 export class FormRegistrationComponent implements OnInit {
-  form: FormGroup;
+  form!: FormGroup;
 
   questions = [
     {
@@ -17,9 +18,24 @@ export class FormRegistrationComponent implements OnInit {
     },
     {
       id: 'empatia',
-      titulo: 'Empatia',
-      descricao: 'A capacidade de se colocar no lugar do outro, entender diferentes perspectivas e emoções é fundamental para a convivência em sociedade e para construir relacionamentos saudáveis.'
-    }
+      title: 'Empatia',
+      description: 'A capacidade de se colocar no lugar do outro, entender diferentes perspectivas e emoções é fundamental para a convivência em sociedade e para construir relacionamentos saudáveis.'
+    },
+    {
+      id: 'comunicacao',
+      title: 'Comunicação',
+      description: 'Habilidades de expressão verbal e não-verbal, escuta ativa, e assertividade são indispensáveis para a construção de relacionamentos e a resolução de conflitos de forma pacífica.'
+    },
+    {
+      id: 'trabalhoEquipe',
+      title: 'Trabalho em equipe',
+      description: 'Promover a cooperação e o espírito de equipe desde cedo ajuda a criança a entender a importância de trabalhar em conjunto para atingir objetivos comuns.'
+    },
+    {
+      id: 'autonomia',
+      title: 'Autonomia',
+      description: 'Ensinar a importância de ser responsável por suas ações e decisões, além de promover a autossuficiência em tarefas adequadas à idade.'
+    },
   ];
 
   options = [
@@ -33,11 +49,14 @@ export class FormRegistrationComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       autoconhecimento: [''],
-      empatia: ['']
+      empatia: [''],
+      comunicacao: [''],
+      trabalhoEquipe: [''],
+      autonomia: ['']
     });
   }
 
-  ngOnInit() {
+  ngOnInit():void {
     // Observar mudanças no formulário se necessário
     this.form.valueChanges.subscribe(values => {
       console.log('Valores do formulário:', values);
